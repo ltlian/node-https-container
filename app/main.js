d908dfe84e638e2e3853a4766dc0e11b474f4673
lars <larstomaslian@gmail.com>
2025-10-19 14:46:27 +0200

    More graceful shutdown handling

--- app/main.js
+++ app/main.js
@@ -8,58 +8,34 @@ const httpsPort = 443;
 /** @type {import("node:tls").SecureContextOptions} */
 const httpsServerOptions = {
   key: readFileSync('./app/certs/rsa-private.key'),
-  cert: readFileSync('./app/certs/x509-signed.crt')
+  cert: readFileSync('./app/certs/x509-signed.crt'),
 };
 
-const app = express()
+const app = express();
 
 app.use(express.static('./app/served'));
 
 const httpServer = app.listen(httpPort, () => {
   console.log(`HTTP server listening on port ${httpPort}`);
-})
+});
 
 const httpsServer = createServer(httpsServerOptions, app);
 httpsServer.listen(httpsPort, () => {
   console.log(`HTTPS server listening on port ${httpsPort}`);
 });
 
-const shutdownHandler = async (signal) => {
+/** @type {NodeJS.SignalsListener} */
+const shutdownHandler = (signal) => {
   console.log(`Received signal '${signal}'. Shutting down servers.`);
 
   try {
-    const timeoutPromise = new Promise((_, reject) => {
-      setTimeout(() => reject(new Error('Shutdown timed out')), 10000);
-    });
-
-    const httpClose = new Promise((resolve, reject) => {
-      httpServer.close((err) => {
-        if (err) reject(err);
-        else {
-          console.log('HTTP server closed.');
-          resolve();
-        }
-      });
-    });
-
-    const httpsClose = new Promise((resolve, reject) => {
-      httpsServer.close((err) => {
-        if (err) reject(err);
-        else {
-          console.log('HTTPS server closed.');
-          resolve();
-        }
-      });
-    });
-
-    await Promise.race([
-      Promise.all([httpClose, httpsClose]),
-      timeoutPromise
-    ]);
-
-    console.log('Exiting.');
-    process.exit(0);
-
+    const timeoutId = setTimeout(() => {
+      throw new Error('Timeout error');
+    }, 5000);
+    httpServer.close();
+    httpsServer.close();
+    process.stdin.pause();
+    clearTimeout(timeoutId);
   } catch (error) {
     console.error('Error during shutdown:', error.message);
     process.exit(1);
@@ -68,3 +44,11 @@ const shutdownHandler = async (signal) => {
 
 process.on('SIGTERM', shutdownHandler);
 process.on('SIGINT', shutdownHandler);
+
+process.stdin.on('data', (data) => {
+  if (data.toString().trim() == 'q') {
+    shutdownHandler('SIGINT');
+  }
+});
+
+console.log("Press CTRL+C or type 'q' to exit.");
